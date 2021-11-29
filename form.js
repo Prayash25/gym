c70ecacaee3dc3aefa6511a1c61240f42b6f4778
Prayash25 <proprayashsatpathy@gmail.com>
2021-11-30 00:32:44 +0530

    completed

--- form.js
+++ form.js
@@ -17,4 +17,67 @@ $('.clear.example .button')
     ;
   })
 ;
+// let selected = [];
+//  userdata = function() {
+ 
+//   for (let option of document.getElementById('plans').options)
+//   {
+//       if (option.selected) {
+//           selected.push(option.value);
+//       }
+//   }
+ 
+// }
+let discountper=0;
+// let total=0;
+let discount=0;
+function selecttimefunc(val)
+{
+  discountper=parseInt(val.options[val.selectedIndex].value,10);
+  console.log(discountper);
+}
+
+ let total= function(sel) {
+  var opts = [],
+    opt;
+    let total=0;
+  var len = sel.options.length;
+  for (var i = 0; i < len; i++) {
+    opt = sel.options[i];
+
+    if (opt.selected) {
+      opts.push(opt);
+      console.log(opt.value);
+      total+=parseInt(opt.value);
+    }
+  }
+  console.log(total);
+  discount= (total*discountper)/100;
+let fp=0;
+fp=total-discount;
+console.log(fp);
+let val=document.getElementById("final");
+// val.style(color: aqua)
+val.innerText= fp;
+val.style.fontSize="20px";
+val.style.color="black"
+val.style.fontWeight="bolder";
+}
+
+
+// function calc(){
+// console.log(typeOf(total), "total")
+
+
+
+
+// let chng= document.getElementById("final");
+// chng.innerText(fp);
+
+// console.log(selected);
+
+
+
+
+
 
---
+++ nav.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+$('.ui.pointing.dropdown')
+  .dropdown()
+  // $("#1mon").click(function(){
+  //   console.log('coupon1 clicked')
+  // });
+  // $("#6mon").click(function(){
+  //   console.log('coupon2 clicked')
+  // });
+  // $("#12mon").click(function(){
+  //   console.log('coupon3 clicked')
+  // });
+});
