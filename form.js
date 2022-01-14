45aa062454f5e32c3871eb875963bf80ef88b7ef
Prayash25 <proprayashsatpathy@gmail.com>
2022-01-14 23:46:55 +0530

    form js is modified (asIwant)

--- form.js
+++ form.js
@@ -29,16 +29,40 @@ $('.clear.example .button')
  
 // }
 let discountper=0;
-// let total=0;
+let tl=0;
 let discount=0;
+function tprice(){
+  if(tl!=0 && discountper!=0){
+    let fp=0;
+    discount= (tl*discountper)/100;
+    fp = tl-discount;
+    let val=document.getElementById("final");
+  // val.style(color: aqua)
+  if(discountper==5){
+    
+    val.innerText= fp;
+  }
+  else if(discountper==10)
+  {
+  val.innerText=fp*6;
+  }
+  else if(discountper==20)
+  {
+    val.innerText=fp*12;
+  }
+  val.style.fontSize="20px";
+  val.style.color="black"
+  val.style.fontWeight="bolder";
+  }
+}
 function selecttimefunc(val)
 {
   discountper=parseInt(val.options[val.selectedIndex].value,10);
-  console.log(discountper);
-  
+  // console.log(discountper);
+  tprice();
 }
 
- let total= function(sel) {
+let total= function(sel) {
   var opts = [],
     opt;
     let total=0;
@@ -48,42 +72,24 @@ function selecttimefunc(val)
 
     if (opt.selected) {
       opts.push(opt);
-      console.log(opt.value);
+      // console.log(opt.value);
       total+=parseInt(opt.value);
     }
   }
-  console.log(total);
-  discount= (total*discountper)/100;
-let fp=0;
-
-fp=total-discount;
-console.log(fp);
-let val=document.getElementById("final");
-// val.style(color: aqua)
-if(discountper==5){
-  
-  val.innerText= fp;
+  // console.log(total);
+// let fp=0;
+tl=total;
+tprice();
+// console.log(fp);
 }
-else if(discountper==10)
-{
-val.innerText=fp*6;
-}
-else if(discountper==20)
-{
-  val.innerText=fp*12;
-}
-val.style.fontSize="20px";
-val.style.color="black"
-val.style.fontWeight="bolder";
-}
-
-
 
 // function calc(){
 // console.log(typeOf(total), "total")
 
 
 
+  
+ 
 
 // let chng= document.getElementById("final");
 // chng.innerText(fp);
